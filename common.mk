c389ee81005b3269406ce0aed94e0fffe06776fe
Steve Kondik <shade@chemlab.org>
2013-07-31 17:26:40 -0700

    display: Fix handling of QCOM_BSP
     * Only set this CFLAG if the device declares TARGET_USES_QCOM_BSP.
    
    Change-Id: Id1c02c5d1cca9016d46005333ba1e2a22e75e1cb
    
    Conflicts:
    	common.mk

--- common.mk
+++ common.mk
@@ -31,6 +31,11 @@ common_deps  :=
 kernel_includes :=
 
 # Executed only on QCOM BSPs
+ifeq ($(TARGET_USES_QCOM_BSP),true)
+    common_flags += -DQCOM_BSP
+endif
+
+
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     common_flags += -DQCOM_BSP
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
